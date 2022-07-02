0d3b3255673a7b65011c9338cb92a80c69b90ba9
Oluwatobi Omotayo <tobitech@ymail.com>
2022-07-03 00:37:58 +0100

    Initial Commit

---
+++ SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Oluwatobi Omotayo on 03/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

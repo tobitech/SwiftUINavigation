d749e72ac520e13f8ba29c79ff6ff584cf90b485
Oluwatobi Omotayo <tobitech@ymail.com>
2022-07-03 00:54:38 +0100

    try to show what swiftui gives us in terms of tab navigation.

--- SwiftUINavigation/ContentView.swift
+++ SwiftUINavigation/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  
+  @State var selection = 1
+  
+  var body: some View {
+    TabView(selection: self.$selection) {
+      Button("Go to Tab 2") {
+        self.selection = 2
+      }
+        .tabItem { Text("One") }
+        .tag(1)
+      
+      Text("Two")
+        .tabItem { Text("Two") }
+        .tag(2)
+      
+      Text("Three")
+        .tabItem { Text("Three") }
+        .tag(3)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView(selection: 3)
+  }
+}
+
+struct ContainerView: View {
+  
+  var selectedTab = 1
+  
+  var body: some View {
+    ContentView(selection: selectedTab)
+  }
 }

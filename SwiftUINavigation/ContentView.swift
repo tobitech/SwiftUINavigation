bea35e3615ff21e5f643a9ae8640b92a10ed46d4
Oluwatobi Omotayo <tobitech@ymail.com>
2022-07-03 01:06:14 +0100

    Replace use of Int for selection with an enum type.
    Also change .tag values for the views.

--- SwiftUINavigation/ContentView.swift
+++ SwiftUINavigation/ContentView.swift
@@ -7,10 +7,15 @@
 
 import SwiftUI
 
+// Selection can be represented with any type that supports Hashable.
+enum Tab {
+  case one, two, three
+}
+
 class AppViewModel: ObservableObject {
-  @Published var selectedTab: Int
+  @Published var selectedTab: Tab
   
-  init(selectedTab: Int = 1) {
+  init(selectedTab: Tab = .one) {
     self.selectedTab = selectedTab
   }
 }
@@ -22,24 +27,24 @@ struct ContentView: View {
   var body: some View {
     TabView(selection: self.$viewModel.selectedTab) {
       Button("Go to Tab 2") {
-        self.viewModel.selectedTab = 2
+        self.viewModel.selectedTab = .two
       }
         .tabItem { Text("One") }
-        .tag(1)
+        .tag(Tab.one)
       
       Text("Two")
         .tabItem { Text("Two") }
-        .tag(2)
+        .tag(Tab.two)
       
       Text("Three")
         .tabItem { Text("Three") }
-        .tag(3)
+        .tag(Tab.three)
     }
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView(viewModel: .init())
+    ContentView(viewModel: .init(selectedTab: .two))
   }
 }
